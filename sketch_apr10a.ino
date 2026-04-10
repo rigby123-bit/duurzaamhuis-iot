723535326877f72f15e798d3cdad4d56420ff2fc
rigby123-bit <victoredwardhendriks@gmail.com>
2026-04-10 13:53:17 +0200

    Refactor sketch_apr10a.ino for clarity

--- sketch_apr10a.ino
+++ sketch_apr10a.ino
@@ -9,13 +9,13 @@ void setup() {
 }
 
 void loop() {
-  // Genereer willekeurige getallen
+  
   A = random(999);
   B = random(999);
   C = random(999);
   D = random(999);
 
-  // Print de waarden
+  
   Serial.println("=== Nieuwe ronde ===");
   Serial.print("A = "); Serial.println(A);
   Serial.print("B = "); Serial.println(B);
@@ -23,7 +23,7 @@ void loop() {
   Serial.print("D = "); Serial.println(D);
   Serial.println();
 
-  // Zet alle LEDs eerst uit
+  
   digitalWrite(3, LOW);
   digitalWrite(5, LOW);
   digitalWrite(6, LOW);
@@ -34,19 +34,19 @@ void loop() {
     Serial.println("D3 AAN (A > B of A < C)");
   }
 
-  // D5 aan: C > A en A < B
+  
   if (C > A && A < B) {
     digitalWrite(5, HIGH);
     Serial.println("D5 AAN (C > A en A < B)");
   }
 
-  // D6 aan: B > C en B > A en C < A
+  
   if (B > C && B > A && C < A) {
     digitalWrite(6, HIGH);
     Serial.println("D6 AAN (B > C en B > A en C < A)");
   }
 
-  // Snel knipperen als D het grootste getal is
+  
   if (D > A && D > B && D > C) {
     Serial.println("D is het grootste! LEDs knipperen...");
     for (int i = 0; i < 10; i++) {
